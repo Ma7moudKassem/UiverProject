e7fdeea6bacefce72bf7e1415dc2c54631a3c42a
Mahmoud Kassem <modykassem123@gmail.com>
2022-05-01 00:09:46 +0200

    first commit

---
+++ Home.js
@@ -0,0 +1,10 @@
+
+
+
+
+var showTouster = false;
+
+function showTouster() {
+    alert("aa")
+    showTouster = true
+};
---
+++ changeBg.js
@@ -0,0 +1,5 @@
+sectionbg.addEventListener("click" , (eo) =>{
+    textArea.style.backgroundColor = `hsl(${Math.round(
+        Math.random() * 360
+        )}, 44%, 55%)`;
+})
